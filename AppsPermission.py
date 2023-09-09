7764f3d9d5b4b4ee39cdb562291b47f6b5151857
Youssef <youssefelbaye@gmail.com>
2023-09-09 19:12:52 +0200

    adding some code to README.md and AppsPermission.py

---
+++ AppsPermission.py
@@ -0,0 +1,8 @@
+print("Welcom to my application")
+age = int(input("How old are you?\n"))
+
+if age < 10:
+    print("Sorry. Your are under age")
+
+else:
+    print("Good. You can use the app")
---
+++ README.md
@@ -0,0 +1,2 @@
+This code is dedicated for building a ban barrier of any apps.
+For any modification you just have to change the parameters.
